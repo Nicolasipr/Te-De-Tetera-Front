782d8d74d4180f946236e5d455862bed8b2835fe
Nicolas Perez Roa <nicolasipr.97@gmail.com>
2019-09-16 03:06:22 -0300

    Adding sample text for About section in  Gloria's  website

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -20,23 +20,46 @@
           </div>
         </b-col>
       </b-row>
-      <b-row class="d-flex justify-content-center">
-        <!-- Start Single Team -->
+      <br>
+      <b-row class="d-flex justify-content-start align-items-center">
+        <!-- Start Single Team Gloria -->
         <b-col lg="4" md="4" sm="6" cols="12">
-          <div class="wn__team">
+          <div class="wn__team ">
             <div class="thumb">
-              <img src="../assets/images/about/team/1.jpg" alt="Team images">
+              <img src="../assets/images/about/team/gloria.jpg" alt="Team images">
             </div>
             <div class="content text-center">
               <h4>{{$t('about.gloriaName')}}</h4>
               <p>{{$t('about.gloriaRole')}}</p>
-<!--               <ul class="team__social__net">
-                <li><a href="#"><FontAwesome :icon="['fab', 'facebook']"></FontAwesome></a></li>
-                <li><a href="#"><FontAwesome :icon="['fab', 'instagram']"></FontAwesome></a></li>
-              </ul> -->
+
             </div>
           </div>
         </b-col>
+        <b-col lg="8" md="8" sm="6" cols="12">
+            <!--          Description -->
+            <div class="text-justify ">
+              <p>
+                Si bien soy Administradora de empresas mi pasión por el dibujo está desde que tengo
+                conciencia, todo lo pintaba, recortaba o intervenía y de manera casi mágica me embarque
+                en este camino de ser una mujer emprendedora cuando Violeta llegó a mi vida.
+
+
+              </p>
+              <br>
+              <p>
+                Cada día me capacito y busco nuevas herramientas para hacer de Te de tetera una gran
+                empresa llena de colores y mucho corazón.
+              </p>
+              <br>
+              <p>
+                Viví rodeada de naturaleza sureña y es esa belleza la que busco plasmar en mis teteras,
+                para que puedas llevar a tu mesa algo único y creado especialmente para ti.
+              </p>
+            </div>
+        </b-col>
+      </b-row>
+      <b-row class="d-flex justify-content-center"  >
+        
         <b-col lg="4" md="4" sm="6" cols="12">
           <div class="wn__team">
             <div class="thumb">
@@ -45,10 +68,6 @@
             <div class="content text-center">
               <h4>{{$t('about.catName')}}</h4>
               <p>{{$t('about.catRole')}}</p>
-<!--               <ul class="team__social__net">
-                <li><a href="#"><FontAwesome :icon="['fab', 'facebook']"></FontAwesome></a></li>
-                <li><a href="#"><FontAwesome :icon="['fab', 'instagram']"></FontAwesome></a></li>
-              </ul> -->
             </div>
           </div>
         </b-col>
@@ -64,4 +83,5 @@ export default {
 </script>
 
 <style lang="css" scoped>
+
 </style>

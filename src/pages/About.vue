0a0c508aaf04bd90ae08a700b11cbd31c011cd2f
Nicolas Perez Roa <nicolasipr.97@gmail.com>
2019-09-16 03:30:07 -0300

    adding i18n Gloria's description + card

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -38,37 +38,75 @@
         <b-col lg="8" md="8" sm="6" cols="12">
             <!--          Description -->
             <div class="text-justify ">
-              <p>
-                Si bien soy Administradora de empresas mi pasión por el dibujo está desde que tengo
-                conciencia, todo lo pintaba, recortaba o intervenía y de manera casi mágica me embarque
-                en este camino de ser una mujer emprendedora cuando Violeta llegó a mi vida.
-
-
-              </p>
+              <p> {{$t('about.gloriaDescription1')}}</p>
               <br>
-              <p>
-                Cada día me capacito y busco nuevas herramientas para hacer de Te de tetera una gran
-                empresa llena de colores y mucho corazón.
-              </p>
+              <p>{{$t('about.gloriaDescription2')}}  </p>
               <br>
-              <p>
-                Viví rodeada de naturaleza sureña y es esa belleza la que busco plasmar en mis teteras,
-                para que puedas llevar a tu mesa algo único y creado especialmente para ti.
-              </p>
+              <p>{{$t('about.gloriaDescription3')}}  </p>
             </div>
         </b-col>
       </b-row>
-      <b-row class="d-flex justify-content-center"  >
-        
+<!--      Benito -->
+      <b-row class="d-flex justify-content-end"  >
+<!--        Description-->
+        <b-col lg="8" md="8" sm="6" cols="12">
+          <!--          Description -->
+          <div class="text-justify ">
+            <p> {{$t('about.gloriaDescription1')}}</p>
+            <br>
+            <p>{{$t('about.gloriaDescription2')}}  </p>
+            <br>
+            <p>{{$t('about.gloriaDescription3')}}  </p>
+          </div>
+        </b-col>
+        <!--        card -->
         <b-col lg="4" md="4" sm="6" cols="12">
           <div class="wn__team">
             <div class="thumb">
-              <img src="../assets/images/about/team/2.jpg" alt="Team images">
+              <img src="../assets/images/about/team/benito.jpg" alt="Team images">
             </div>
             <div class="content text-center">
               <h4>{{$t('about.catName')}}</h4>
               <p>{{$t('about.catRole')}}</p>
             </div>
+          </div>
+        </b-col>
+      </b-row>
+<!--      Viole -->
+      <b-row class="d-flex justify-content-start align-items-center">
+        <!-- Start Single Team Gloria -->
+        <b-col lg="4" md="4" sm="6" cols="12">
+          <div class="wn__team ">
+            <div class="thumb">
+              <img src="../assets/images/about/team/viole.jpg" alt="Team images">
+            </div>
+            <div class="content text-center">
+              <h4>{{$t('about.gloriaName')}}</h4>
+              <p>{{$t('about.gloriaRole')}}</p>
+
+            </div>
+          </div>
+        </b-col>
+        <b-col lg="8" md="8" sm="6" cols="12">
+          <!--          Description -->
+          <div class="text-justify ">
+            <p>
+              Si bien soy Administradora de empresas mi pasión por el dibujo está desde que tengo
+              conciencia, todo lo pintaba, recortaba o intervenía y de manera casi mágica me embarque
+              en este camino de ser una mujer emprendedora cuando Violeta llegó a mi vida.
+
+
+            </p>
+            <br>
+            <p>
+              Cada día me capacito y busco nuevas herramientas para hacer de Te de tetera una gran
+              empresa llena de colores y mucho corazón.
+            </p>
+            <br>
+            <p>
+              Viví rodeada de naturaleza sureña y es esa belleza la que busco plasmar en mis teteras,
+              para que puedas llevar a tu mesa algo único y creado especialmente para ti.
+            </p>
           </div>
         </b-col>
       </b-row>
